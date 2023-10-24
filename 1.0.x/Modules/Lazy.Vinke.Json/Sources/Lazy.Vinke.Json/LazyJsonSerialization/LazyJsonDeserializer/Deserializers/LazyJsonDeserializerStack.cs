019e171796a9f2b98d59dc3c0bfe06b1417cd46d
Isaac Saraiva <isaacsaraiva.vinke@outlook.com>
2023-10-24 20:44:00 -0300

    1. Created LazyJsonSerializerStack 2. Created LazyJsonDeserializerStack

---
+++ 1.0.x/Modules/Lazy.Vinke.Json/Sources/Lazy.Vinke.Json/LazyJsonSerialization/LazyJsonDeserializer/Deserializers/LazyJsonDeserializerStack.cs
@@ -0,0 +1,85 @@
+﻿// LazyJsonDeserializerStack.cs
+//
+// This file is integrated part of "Lazy Vinke Json" solution
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2023, October 24
+
+using System;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Lazy.Vinke.Json
+{
+    public class LazyJsonDeserializerStack : LazyJsonDeserializerBase
+    {
+        #region Variables
+        #endregion Variables
+
+        #region Constructors
+        #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        /// Deserialize the json token to an object
+        /// </summary>
+        /// <param name="jsonToken">The json token</param>
+        /// <param name="dataType">The type of the object</param>
+        /// <param name="jsonDeserializerOptions">The json deserializer options</param>
+        /// <returns>The deserialized object</returns>
+        public override Object Deserialize(LazyJsonToken jsonToken, Type dataType, LazyJsonDeserializerOptions jsonDeserializerOptions = null)
+        {
+            if (jsonToken != null && jsonToken.Type == LazyJsonType.Array && dataType != null && dataType.IsGenericType == true && dataType.GetGenericTypeDefinition() == typeof(Stack<>))
+            {
+                LazyJsonArray jsonArray = (LazyJsonArray)jsonToken;
+
+                Object dataStack = Activator.CreateInstance(dataType);
+                MethodInfo methodInfoPush = dataType.GetMethods().First(x => x.Name == "Push");
+
+                LazyJsonDeserializerBase jsonDeserializer = null;
+                LazyJsonDeserializeTokenEventHandler jsonDeserializeTokenEventHandler = null;
+
+                Type jsonDeserializerType = LazyJsonDeserializer.SelectDeserializerType(dataType.GenericTypeArguments[0], jsonDeserializerOptions);
+
+                if (jsonDeserializerType != null)
+                {
+                    jsonDeserializer = (LazyJsonDeserializerBase)Activator.CreateInstance(jsonDeserializerType);
+                    jsonDeserializeTokenEventHandler = new LazyJsonDeserializeTokenEventHandler(jsonDeserializer.Deserialize);
+                }
+                else
+                {
+                    jsonDeserializeTokenEventHandler = new LazyJsonDeserializeTokenEventHandler(LazyJsonDeserializer.DeserializeToken);
+                }
+
+                LazyJsonDeserializerOptions options = jsonDeserializerOptions != null ? jsonDeserializerOptions : new LazyJsonDeserializerOptions();
+                LazyJsonDeserializerOptionsStack optionsStack = options.Contains<LazyJsonDeserializerOptionsStack>() == true ? options.Item<LazyJsonDeserializerOptionsStack>() : new LazyJsonDeserializerOptionsStack();
+
+                if (optionsStack.ReadReverse == true)
+                {
+                    for (int index = (jsonArray.Length - 1); index >= 0; index--)
+                        methodInfoPush.Invoke(dataStack, new Object[] { jsonDeserializeTokenEventHandler(jsonArray[index], dataType.GenericTypeArguments[0], jsonDeserializerOptions) });
+                }
+                else
+                {
+                    for (int index = 0; index < jsonArray.Length; index++)
+                        methodInfoPush.Invoke(dataStack, new Object[] { jsonDeserializeTokenEventHandler(jsonArray[index], dataType.GenericTypeArguments[0], jsonDeserializerOptions) });
+                }
+
+                return dataStack;
+            }
+
+            return null;
+        }
+
+        #endregion Methods
+
+        #region Properties
+        #endregion Properties
+    }
+}
---
+++ 1.0.x/Modules/Lazy.Vinke.Json/Sources/Lazy.Vinke.Json/LazyJsonSerialization/LazyJsonSerializer/Serializers/LazyJsonSerializerStack.cs
@@ -0,0 +1,91 @@
+﻿// LazyJsonSerializerStack.cs
+//
+// This file is integrated part of "Lazy Vinke Json" solution
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2023, October 24
+
+using System;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace Lazy.Vinke.Json
+{
+    public class LazyJsonSerializerStack : LazyJsonSerializerBase
+    {
+        #region Variables
+        #endregion Variables
+
+        #region Constructors
+        #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        /// Serialize an object to a json token
+        /// </summary>
+        /// <param name="data">The object to be serialized</param>
+        /// <param name="jsonSerializerOptions">The json serializer options</param>
+        /// <returns>The json token</returns>
+        public override LazyJsonToken Serialize(Object data, LazyJsonSerializerOptions jsonSerializerOptions = null)
+        {
+            if (data != null)
+            {
+                Type dataType = data.GetType();
+
+                if (dataType.IsGenericType == true && dataType.GetGenericTypeDefinition() == typeof(Stack<>))
+                {
+                    Int32 count = (Int32)dataType.GetProperties().First(x => x.Name == "Count").GetValue(data);
+                    MethodInfo methodInfoPop = dataType.GetMethods().First(x => x.Name == "Pop");
+
+                    LazyJsonArray jsonArray = new LazyJsonArray();
+
+                    for (int index = 0; index < count; index++)
+                        jsonArray.Add(new LazyJsonNull());
+
+                    LazyJsonSerializerBase jsonSerializer = null;
+                    LazyJsonSerializeTokenEventHandler jsonSerializeTokenEventHandler = null;
+
+                    Type jsonSerializerType = LazyJsonSerializer.SelectSerializerType(dataType.GenericTypeArguments[0], jsonSerializerOptions);
+
+                    if (jsonSerializerType != null)
+                    {
+                        jsonSerializer = (LazyJsonSerializerBase)Activator.CreateInstance(jsonSerializerType);
+                        jsonSerializeTokenEventHandler = new LazyJsonSerializeTokenEventHandler(jsonSerializer.Serialize);
+                    }
+                    else
+                    {
+                        jsonSerializeTokenEventHandler = new LazyJsonSerializeTokenEventHandler(LazyJsonSerializer.SerializeToken);
+                    }
+
+                    LazyJsonSerializerOptions options = jsonSerializerOptions != null ? jsonSerializerOptions : new LazyJsonSerializerOptions();
+                    LazyJsonSerializerOptionsStack optionsStack = options.Contains<LazyJsonSerializerOptionsStack>() == true ? options.Item<LazyJsonSerializerOptionsStack>() : new LazyJsonSerializerOptionsStack();
+
+                    if (optionsStack.WriteReverse == true)
+                    {
+                        for (int index = (count - 1); index >= 0; index--)
+                            jsonArray[index] = jsonSerializeTokenEventHandler(methodInfoPop.Invoke(data, null), jsonSerializerOptions);
+                    }
+                    else
+                    {
+                        for (int index = 0; index < count; index++)
+                            jsonArray[index] = jsonSerializeTokenEventHandler(methodInfoPop.Invoke(data, null), jsonSerializerOptions);
+                    }
+
+                    return jsonArray;
+                }
+            }
+
+            return new LazyJsonNull();
+        }
+
+        #endregion Methods
+
+        #region Properties
+        #endregion Properties
+    }
+}
