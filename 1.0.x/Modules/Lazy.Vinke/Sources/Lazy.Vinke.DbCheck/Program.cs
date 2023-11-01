6d9da2a23adb93e779b536837c2dde4c9e1ac684
Isaac Saraiva <isaacsaraiva.vinke@outlook.com>
2023-11-01 19:52:11 -0300

    1. Created LazyAssemblyResolver to resolve assemblies on runtime environment directory 2. Implemented Lazy.Vinke.DbCheck to test connection to databases

--- 1.0.x/Modules/Lazy.Vinke/Sources/Lazy.Vinke.DbCheck/Program.cs
+++ 1.0.x/Modules/Lazy.Vinke/Sources/Lazy.Vinke.DbCheck/Program.cs
@@ -7,6 +7,11 @@
 // Created on 2023, November 01
 
 using System;
+using System.IO;
+using System.Reflection;
+
+using Lazy.Vinke;
+using Lazy.Vinke.Database;
 
 namespace Lazy.Vinke.DbCheck
 {
@@ -15,7 +20,79 @@ namespace Lazy.Vinke.DbCheck
         [STAThread()]
         public static void Main(String[] args)
         {
-            Console.WriteLine("Hello World!");
+            AppDomain.CurrentDomain.AssemblyResolve += LazyAssemblyResolver.Resolve;
+
+            Console.WriteLine();
+            Console.WriteLine(" - Which database would you like to connect?");
+            Console.WriteLine();
+            Console.WriteLine("\t1. MySql");
+            Console.WriteLine("\t2. Oracle");
+            Console.WriteLine("\t3. Postgre");
+            Console.WriteLine("\t4. SqlServer");
+            Console.WriteLine("\t0. Give up!");
+            Console.WriteLine();
+
+            Console.Write("\tYour option is: ");
+            Char ch = (Char)Console.Read();
+            Console.WriteLine();
+
+            switch (ch)
+            {
+                case '1': ConnectOnMySql(); break;
+                case '2': ConnectOnOracle(); break;
+                case '3': ConnectOnPostgre(); break;
+                case '4': ConnectOnSqlServer(); break;
+                default:
+                    Console.WriteLine("\tGood bye!");
+                    Console.WriteLine();
+                    break;
+            }
+        }
+
+        private static void ConnectOnMySql()
+        {
+            String connectionString = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "Config", "Lazy.Vinke.DbCheck.Connection.MySql.txt"));
+            TryConnect("Lazy.Vinke.Database.MySql.dll", "Lazy.Vinke.Database.MySql.LazyDatabaseMySql", connectionString, "MySql");
+        }
+
+        private static void ConnectOnOracle()
+        {
+            String connectionString = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "Config", "Lazy.Vinke.DbCheck.Connection.Oracle.txt"));
+            TryConnect("Lazy.Vinke.Database.Oracle.dll", "Lazy.Vinke.Database.Oracle.LazyDatabaseOracle", connectionString, "Oracle");
+        }
+
+        private static void ConnectOnPostgre()
+        {
+            String connectionString = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "Config", "Lazy.Vinke.DbCheck.Connection.Postgre.txt"));
+            TryConnect("Lazy.Vinke.Database.Postgre.dll", "Lazy.Vinke.Database.Postgre.LazyDatabasePostgre", connectionString, "Postgre");
+        }
+
+        private static void ConnectOnSqlServer()
+        {
+            String connectionString = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "Config", "Lazy.Vinke.DbCheck.Connection.SqlServer.txt"));
+            TryConnect("Lazy.Vinke.Database.SqlServer.dll", "Lazy.Vinke.Database.SqlServer.LazyDatabaseSqlServer", connectionString, "SqlServer");
+        }
+
+        private static void TryConnect(String assemblyName, String classFullName, String connectionString, String dbmsName)
+        {
+            try
+            {
+                Type type = Assembly.Load(assemblyName).GetType(classFullName);
+                LazyDatabase database = (LazyDatabase)Activator.CreateInstance(type, new Object[] { connectionString, null });
+
+                database.OpenConnection();
+                database.CloseConnection();
+
+                Console.WriteLine(String.Format("\tConnected successfully on {0}!", dbmsName));
+                Console.WriteLine();
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(String.Format("\tUnable to connect on {0}!", dbmsName));
+                Console.WriteLine();
+                Console.WriteLine(exp.GetBaseException().Message);
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ 1.0.x/Modules/Lazy.Vinke/Sources/Lazy.Vinke/LazyAssemblyResolver.cs
@@ -0,0 +1,74 @@
+﻿// LazyAssemblyResolver.cs
+//
+// This file is integrated part of "Lazy Vinke" solution
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2023, November 01
+
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Lazy.Vinke
+{
+    public static class LazyAssemblyResolver
+    {
+        #region Variables
+        #endregion Variables
+
+        #region Methods
+
+        /// <summary>
+        /// Resolve an assembly location
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="args">The resolve event arguments</param>
+        /// <returns>The located assembly</returns>
+        public static Assembly Resolve(Object sender, ResolveEventArgs args)
+        {
+            String assemblyFileName = args.Name.Substring(0, args.Name.IndexOf(','));
+
+            if (assemblyFileName.EndsWith(".dll") == true)
+                assemblyFileName = assemblyFileName.Remove(assemblyFileName.LastIndexOf(".dll"), 4);
+
+            String assemblyFolderPath = Path.Combine(Environment.CurrentDirectory, "Bin", assemblyFileName.ToLower());
+
+            if (Directory.Exists(assemblyFolderPath) == false)
+                assemblyFolderPath = Path.Combine(Environment.CurrentDirectory, "Bin");
+
+            String[] fileCollection = Directory.GetFiles(assemblyFolderPath, assemblyFileName + ".dll", SearchOption.AllDirectories);
+
+            if (args.Name.Contains("Version") == true && args.Name.Contains("Culture") == true && args.Name.Contains("PublicKeyToken") == true)
+            {
+                foreach (String file in fileCollection)
+                {
+                    try
+                    {
+                        Assembly assembly = Assembly.LoadFrom(file);
+
+                        if (assembly.GetName().FullName == args.Name)
+                            return assembly;
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            if (fileCollection.Length > 0)
+            {
+                try { return Assembly.LoadFrom(fileCollection[0]); }
+                catch { return null; }
+            }
+
+            return null;
+        }
+
+        #endregion Methods
+
+        #region Properties
+        #endregion Properties
+    }
+}
