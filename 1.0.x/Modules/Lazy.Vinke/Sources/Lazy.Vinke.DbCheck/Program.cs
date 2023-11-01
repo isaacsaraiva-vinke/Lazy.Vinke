b4b0d13155770c1efb32724f8dac69d73ff1debe
Isaac Saraiva <isaacsaraiva.vinke@outlook.com>
2023-11-01 14:39:29 -0300

    1. Created Lazy.Vinke.DbCheck solution

---
+++ 1.0.x/Modules/Lazy.Vinke/Sources/Lazy.Vinke.DbCheck/Program.cs
@@ -0,0 +1,21 @@
+﻿// Program.cs
+//
+// This file is integrated part of "Lazy Vinke DbCheck" solution
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2023, November 01
+
+using System;
+
+namespace Lazy.Vinke.DbCheck
+{
+    public static class Program
+    {
+        [STAThread()]
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 1.0.x/Modules/Lazy.Vinke/Sources/Lazy.Vinke.DbCheck/Properties/Miscellaneous/Lazy.Vinke.DbCheck.sh
@@ -0,0 +1,1 @@
+﻿dotnet ./Lazy.Vinke.DbCheck.dll
