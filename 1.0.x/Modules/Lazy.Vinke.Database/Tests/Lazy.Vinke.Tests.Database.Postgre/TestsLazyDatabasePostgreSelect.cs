de056083f3227de73a33e479192d3d2a4af1328b
Isaac Saraiva <isaacsaraiva.vinke@outlook.com>
2023-11-09 19:29:08 -0300

    1. Added new test cases

--- 1.0.x/Modules/Lazy.Vinke.Database/Tests/Lazy.Vinke.Tests.Database.Postgre/TestsLazyDatabasePostgreSelect.cs
+++ 1.0.x/Modules/Lazy.Vinke.Database/Tests/Lazy.Vinke.Tests.Database.Postgre/TestsLazyDatabasePostgreSelect.cs
@@ -164,6 +164,115 @@ namespace Lazy.Vinke.Tests.Database.Postgre
         }
 
         [TestMethod]
+        public virtual void Select_QueryTable_Arrays_Success()
+        {
+            // Arrange
+            String tableName = "TestsSelectQueryTable";
+            String columnsName = "Id, Name, Amount";
+            String columnsParameter = "@Id, @Name, @Amount";
+            String sqlDelete = "delete from " + tableName + " where Id in (4000,5000,6000)";
+            String sqlInsert = "insert into " + tableName + " (" + columnsName + ") values (" + columnsParameter + ")";
+            try { this.Database.Execute(sqlDelete, null); }
+            catch { /* Just to be sure that the table will be empty */ }
+
+            String[] fields = new String[] { "Id", "Name", "Amount" };
+            NpgsqlDbType[] dbTypes = new NpgsqlDbType[] { NpgsqlDbType.Integer, NpgsqlDbType.Varchar, NpgsqlDbType.Numeric };
+            List<Object[]> valuesList = new List<Object[]>() {
+                new Object[] { 4000, "Item 4000", 4000.1m },
+                new Object[] { 5000, "Item 5000", 5000.1m },
+                new Object[] { 6000, "Item 6000", 6000.1m }
+            };
+
+            LazyDatabasePostgre databasePostgre = (LazyDatabasePostgre)this.Database;
+
+            databasePostgre.Execute(sqlInsert, valuesList[0], dbTypes, fields);
+            databasePostgre.Execute(sqlInsert, valuesList[1], dbTypes, fields);
+            databasePostgre.Execute(sqlInsert, valuesList[2], dbTypes, fields);
+
+            // Act
+            DataTable dataTable = databasePostgre.Select(tableName, valuesList[1], dbTypes, fields, returnFields: new String[] { "Amount" });
+
+            // Assert
+            Assert.AreEqual(dataTable.TableName, tableName);
+            Assert.IsTrue(dataTable.Rows.Count == 1);
+            Assert.AreEqual(Convert.ToDecimal(dataTable.Rows[0]["Amount"]), 5000.1m);
+
+            // Clean
+            try { this.Database.Execute(sqlDelete, null); }
+            catch { /* Just to be sure that the table will be empty */ }
+        }
+
+        [TestMethod]
+        public virtual void Select_QueryPage_Arrays_Success()
+        {
+            // Arrange
+            String tableName = "TestsSelectQueryPage";
+            String columnsName = "IdMaster, IdChild, Name, Amount";
+            String columnsParameter = "@IdMaster, @IdChild, @Name, @Amount";
+            String sqlDelete = "delete from " + tableName + " where IdMaster in (300,325,375)";
+            String sqlInsert = "insert into " + tableName + " (" + columnsName + ") values (" + columnsParameter + ")";
+            try { this.Database.Execute(sqlDelete, null); }
+            catch { /* Just to be sure that the table will be empty */ }
+
+            String[] fields = new String[] { "IdMaster", "IdChild", "Name", "Amount" };
+            NpgsqlDbType[] dbTypes = new NpgsqlDbType[] { NpgsqlDbType.Integer, NpgsqlDbType.Integer, NpgsqlDbType.Varchar, NpgsqlDbType.Numeric };
+            List<Object[]> valuesList = new List<Object[]>() {
+                new Object[] { 300, 1000, "Item 1000", 1000.1m },
+                new Object[] { 300, 2000, "Item 2000", 2000.2m },
+                new Object[] { 300, 3000, "Item 3000", 3000.3m },
+                new Object[] { 325, 1000, "Item 1000", 1000.1m },
+                new Object[] { 375, 2000, "Item 2000", 2000.2m }
+            };
+
+            LazyDatabasePostgre databasePostgre = (LazyDatabasePostgre)this.Database;
+
+            databasePostgre.Execute(sqlInsert, valuesList[0], dbTypes, fields);
+            databasePostgre.Execute(sqlInsert, valuesList[1], dbTypes, fields);
+            databasePostgre.Execute(sqlInsert, valuesList[2], dbTypes, fields);
+            databasePostgre.Execute(sqlInsert, valuesList[3], dbTypes, fields);
+            databasePostgre.Execute(sqlInsert, valuesList[4], dbTypes, fields);
+
+            fields = new String[] { "IdMaster" };
+            dbTypes = new NpgsqlDbType[] { NpgsqlDbType.Integer };
+            valuesList = new List<Object[]>() { new Object[] { 300 } };
+
+            LazyPageData pageData = new LazyPageData() { PageSize = 2, OrderBy = "IdMaster,IdChild" };
+
+            // Act
+            pageData.PageNum = 1;
+            LazyPageResult pageResult1 = databasePostgre.Select(tableName, pageData, valuesList[0], dbTypes, fields, returnFields: new String[] { "IdMaster", "IdChild", "Amount" });
+            pageData.PageNum = 2;
+            LazyPageResult pageResult2 = databasePostgre.Select(tableName, pageData, valuesList[0], dbTypes, fields, returnFields: new String[] { "IdMaster", "IdChild", "Amount" });
+
+            // Assert
+            Assert.AreEqual(pageResult1.PageNum, 1);
+            Assert.AreEqual(pageResult1.PageSize, 2);
+            Assert.AreEqual(pageResult1.PageItems, 2);
+            Assert.AreEqual(pageResult1.PageCount, 2);
+            Assert.AreEqual(pageResult1.CurrentCount, 2);
+            Assert.AreEqual(pageResult1.TotalCount, 3);
+            Assert.AreEqual(pageResult1.HasNextPage, true);
+            Assert.AreEqual(pageResult1.DataTable.TableName, tableName);
+            Assert.IsTrue(pageResult1.DataTable.Rows.Count == 2);
+            Assert.AreEqual(Convert.ToDecimal(pageResult1.DataTable.Rows[0]["Amount"]), 1000.1m);
+            Assert.AreEqual(Convert.ToDecimal(pageResult1.DataTable.Rows[1]["Amount"]), 2000.2m);
+            Assert.AreEqual(pageResult2.PageNum, 2);
+            Assert.AreEqual(pageResult2.PageSize, 2);
+            Assert.AreEqual(pageResult2.PageItems, 1);
+            Assert.AreEqual(pageResult2.PageCount, 2);
+            Assert.AreEqual(pageResult2.CurrentCount, 3);
+            Assert.AreEqual(pageResult2.TotalCount, 3);
+            Assert.AreEqual(pageResult2.HasNextPage, false);
+            Assert.AreEqual(pageResult2.DataTable.TableName, tableName);
+            Assert.IsTrue(pageResult2.DataTable.Rows.Count == 1);
+            Assert.AreEqual(Convert.ToDecimal(pageResult2.DataTable.Rows[0]["Amount"]), 3000.3m);
+
+            // Clean
+            try { this.Database.Execute(sqlDelete, null); }
+            catch { /* Just to be sure that the table will be empty */ }
+        }
+
+        [TestMethod]
         public override void Select_Validations_QueryTableDataRow_Exception()
         {
             base.Select_Validations_QueryTableDataRow_Exception();
